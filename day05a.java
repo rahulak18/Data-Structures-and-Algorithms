5ef275b776656bacd9dc67801c396d513facedf7
rahulak18 <rahulakannur19@gmail.com>
2024-02-24 17:42:28 +0530

    Is palindrome array or not

---
+++ day05a.java
@@ -0,0 +1,31 @@
+
+public class day05a {
+
+    public static void main(String[] args) {
+
+        int[] arr = { 121, 232, 343, 545, 545 };
+
+        System.out.println(palindromeArray(arr) ? "Yes it is an palindrome array" : "No it is not an palindrome array");
+    }
+
+    public static int reverseNumber(int n) {
+        int finalValue = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+            finalValue = finalValue * 10 + digit;
+            n /= 10;
+        }
+        return finalValue;
+    }
+
+    public static boolean palindromeArray(int[] arr) {
+        for (int i : arr) {
+            if (i != reverseNumber(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
